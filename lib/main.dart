0b761dd39381d9908f17946f05064f41e6f08fb1
GUSTAVO GEWEHR SOARES <52550134+gusgewehr@users.noreply.github.com>
2023-01-08 01:04:03 -0300

    added store function

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -53,32 +54,74 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+class Upgrade {
+  String name;
+  int value;
+  int perk;
+  String effect;
+  String image;
+  bool available;
+
+  Upgrade (this.name, this.value, this.perk, this.effect,this.image, this.available);
+}
+
 class _HomePageState extends State<HomePage> {
 
-  String assetImage = 'images/positivo.png';
+  String assetImage = 'images/weapons/hjke11.webp';
 
   int money = 0;
 
-  void _incrementMoney(){
-    setState(() {
-      money++;
-    });
+  List<Upgrade> listUpgrades = [
+    Upgrade('Skippy', 100, 9,'+ \$9 every click','images/weapons/skippy.webp', true),
+    Upgrade('Achiles', 150, 20,'+ \$20 every click','images/weapons/achiles.webp', true),
+    Upgrade('Kyubi', 500, 50,'+ \$50 every click','images/weapons/kyubi.webp', true),
+    Upgrade('Prejudice', 1000, 100,'+ \$100 every click','images/weapons/prejudice.webp', true),
+    Upgrade('HMG', 3000, 150,'+ \$150 every click','images/weapons/hmg.webp', true),
+  ];
 
+
+  void _incrementMoney(){
+    int increaseBy = 1;
+
+    for(Upgrade upgrade in listUpgrades){
+      if(upgrade.available == false){
+        increaseBy = increaseBy + upgrade.perk;
+      }
+    }
+    setState(() {
+      money = money + increaseBy;
+    });
   }
 
+  void _addUpgrade(int index,int value, String image){
+    if(money >= value){
+      listUpgrades.elementAt(index).available = false;
+      setState(() {
+        money = money - value;
+        assetImage = image;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
       body: Center(
         child: Column(
+          mainAxisSize: MainAxisSize.max,
           children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
-              onPressed: _incrementMoney,
-              child: Image.asset(assetImage),
+            SizedBox(
+              height: screenHeight * 0.8,
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                onPressed: _incrementMoney,
+                child: Image.asset(assetImage),
+                ),
               ),
             ),
             Container(
@@ -96,7 +139,50 @@ class _HomePageState extends State<HomePage> {
                   Expanded(
                     flex: 1,
                     child: TextButton(
-                      onPressed: _incrementMoney,
+                      onPressed: () => showDialog<String>(
+                        context: context,
+                        builder: (BuildContext context) {
+                        return SimpleDialog(
+                          title: const Text('Power UPs Store', style: TextStyle(color: Color(0xFF2BF0FB)),),
+                          backgroundColor: const Color(0xFF024059),
+
+                          children: <Widget>[
+                            for(Upgrade upgrade in listUpgrades)
+                              if(upgrade.available == true)
+                                SimpleDialogOption(
+                                  onPressed: () {
+                                    _addUpgrade(listUpgrades.indexOf(upgrade), upgrade.value, upgrade.image);
+                                    Navigator.pop(context);
+                                  },
+                                  child: Row(
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      Image.asset(upgrade.image, width: screenWidth * 0.3 ,),
+
+                                      Column(
+                                        crossAxisAlignment: CrossAxisAlignment.center,
+                                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                        children: [
+                                          Row(
+                                            crossAxisAlignment: CrossAxisAlignment.center,
+                                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                            children: [
+                                              Text(upgrade.name, style: TextStyle(fontSize: 18),),
+
+                                              Text('    \$${upgrade.value}', style: TextStyle(fontSize: 18),),
+                                            ],
+                                          ),
+                                          Text(upgrade.effect, style: TextStyle(fontSize: 12, fontWeight: FontWeight.normal, fontFamily: 'Roboto'),)
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                ),
+                            ],
+                          );
+                        }
+                      ),
                       style: TextButton.styleFrom(
                         backgroundColor: const Color(0xFF2E86AB),
                         foregroundColor: const Color(0xFF2BF0FB)
@@ -104,23 +190,14 @@ class _HomePageState extends State<HomePage> {
                       child: const Text('Store', style: TextStyle( fontSize: 28)),
                     ),
                   ),
-                  Expanded(
-                    flex: 1,
-                    child: TextButton(
-                      onPressed: _incrementMoney,
-                      style: TextButton.styleFrom(
-                          backgroundColor: const Color(0xFF2E86AB),
-                          foregroundColor: const Color(0xFF2BF0FB)
-                      ),
-                      child: const Text('Config', style: TextStyle( fontSize: 28)),
-                    ),
-                  ),
+
                 ],
               ),
             )
           ],
         ),
-      )
+      ),
+
     );
 
   }
